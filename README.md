c622db614a6c9df2b680209c5cbd2a52a17197e0
fer-oshiro <fernanda.sayuri@outlook.com>
2022-06-26 21:51:07 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Genshin Artifact com ML Kit
+
+Projeto com base no [react-native-mlkit-tutorial](https://github.com/dimaportenko/react-native-mlkit-tutorial) que usa Google ML Kit API.
---
+++ ios/MLKit/RCTTextRecognitionModule.h
@@ -0,0 +1,9 @@
+#ifndef RCTTextRecognitionModule_h
+#define RCTTextRecognitionModule_h
+
+#import <React/RCTBridgeModule.h>
+@interface RCTTextRecognitionModule : NSObject <RCTBridgeModule>
+
+@end
+
+#endif /* RCTTextRecognitionModule_h */
